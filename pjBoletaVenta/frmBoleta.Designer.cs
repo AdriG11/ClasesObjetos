d612723da51f1e857603bfae1b75fb08c0459ab9
AdriG11 <gaitanadriana71@gmail.com>
2023-08-24 12:05:44 -0600

    actuslizacion

--- pjBoletaVenta/frmBoleta.Designer.cs
+++ pjBoletaVenta/frmBoleta.Designer.cs
@@ -30,7 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
@@ -43,26 +43,26 @@
             this.btnRegistrar = new System.Windows.Forms.Button();
             this.label12 = new System.Windows.Forms.Label();
             this.lvEstadistica = new System.Windows.Forms.ListView();
-            this.btnTotal = new System.Windows.Forms.Label();
-            this.label14 = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label9 = new System.Windows.Forms.Label();
-            this.cboProductos = new System.Windows.Forms.ListBox();
-            this.label10 = new System.Windows.Forms.Label();
-            this.txtPrecio = new System.Windows.Forms.TextBox();
-            this.label11 = new System.Windows.Forms.Label();
-            this.txtCantidad = new System.Windows.Forms.TextBox();
-            this.lvDetalles = new System.Windows.Forms.ListView();
-            this.btnAgregar = new System.Windows.Forms.Button();
-            this.btnSalir = new System.Windows.Forms.Button();
-            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
-            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
-            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
-            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
             this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
             this.columnHeader6 = new System.Windows.Forms.ColumnHeader();
             this.columnHeader7 = new System.Windows.Forms.ColumnHeader();
             this.columnHeader8 = new System.Windows.Forms.ColumnHeader();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cboProductos = new System.Windows.Forms.ComboBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lvDetalles = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.btnSalir = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -85,14 +85,14 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "BOLETA DE VENTA";
             // 
-            // label3
+            // lblNumero
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(680, 89);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(37, 15);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "00000";
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(689, 86);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(37, 15);
+            this.lblNumero.TabIndex = 2;
+            this.lblNumero.Text = "00000";
             // 
             // label4
             // 
@@ -103,7 +103,6 @@
             this.label4.Size = new System.Drawing.Size(292, 37);
             this.label4.TabIndex = 3;
             this.label4.Text = "CONTROL DE VENTAS";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
             // 
@@ -186,7 +185,6 @@
             this.label12.Size = new System.Drawing.Size(187, 15);
             this.label12.TabIndex = 14;
             this.label12.Text = "RESUMEN DE REGISRO DE BOLETA";
-            this.label12.Click += new System.EventHandler(this.label12_Click);
             // 
             // lvEstadistica
             // 
@@ -202,144 +200,6 @@
             this.lvEstadistica.TabIndex = 15;
             this.lvEstadistica.UseCompatibleStateImageBehavior = false;
             this.lvEstadistica.View = System.Windows.Forms.View.Details;
-            // 
-            // btnTotal
-            // 
-            this.btnTotal.AutoSize = true;
-            this.btnTotal.Location = new System.Drawing.Point(517, 391);
-            this.btnTotal.Name = "btnTotal";
-            this.btnTotal.Size = new System.Drawing.Size(39, 15);
-            this.btnTotal.TabIndex = 16;
-            this.btnTotal.Text = "TOTAL";
-            // 
-            // label14
-            // 
-            this.label14.AutoSize = true;
-            this.label14.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label14.Location = new System.Drawing.Point(698, 391);
-            this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(47, 15);
-            this.label14.TabIndex = 17;
-            this.label14.Text = "label14";
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.btnAgregar);
-            this.groupBox1.Controls.Add(this.txtCantidad);
-            this.groupBox1.Controls.Add(this.label11);
-            this.groupBox1.Controls.Add(this.txtPrecio);
-            this.groupBox1.Controls.Add(this.label10);
-            this.groupBox1.Controls.Add(this.cboProductos);
-            this.groupBox1.Controls.Add(this.label9);
-            this.groupBox1.Location = new System.Drawing.Point(14, 192);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(792, 90);
-            this.groupBox1.TabIndex = 18;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "DATOS DEL PRODUCTO";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(8, 21);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(68, 15);
-            this.label9.TabIndex = 0;
-            this.label9.Text = "PRODUCTO";
-            // 
-            // cboProductos
-            // 
-            this.cboProductos.FormattingEnabled = true;
-            this.cboProductos.ItemHeight = 15;
-            this.cboProductos.Location = new System.Drawing.Point(3, 50);
-            this.cboProductos.Name = "cboProductos";
-            this.cboProductos.Size = new System.Drawing.Size(109, 19);
-            this.cboProductos.TabIndex = 1;
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(305, 21);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(47, 15);
-            this.label10.TabIndex = 2;
-            this.label10.Text = "PRECIO";
-            // 
-            // txtPrecio
-            // 
-            this.txtPrecio.Location = new System.Drawing.Point(275, 46);
-            this.txtPrecio.Name = "txtPrecio";
-            this.txtPrecio.Size = new System.Drawing.Size(110, 23);
-            this.txtPrecio.TabIndex = 3;
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(543, 19);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(65, 15);
-            this.label11.TabIndex = 4;
-            this.label11.Text = "CANTIDAD";
-            // 
-            // txtCantidad
-            // 
-            this.txtCantidad.Location = new System.Drawing.Point(527, 46);
-            this.txtCantidad.Name = "txtCantidad";
-            this.txtCantidad.Size = new System.Drawing.Size(115, 23);
-            this.txtCantidad.TabIndex = 5;
-            // 
-            // lvDetalles
-            // 
-            this.lvDetalles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader1,
-            this.columnHeader2,
-            this.columnHeader3,
-            this.columnHeader4});
-            this.lvDetalles.GridLines = true;
-            this.lvDetalles.Location = new System.Drawing.Point(7, 288);
-            this.lvDetalles.Name = "lvDetalles";
-            this.lvDetalles.Size = new System.Drawing.Size(781, 100);
-            this.lvDetalles.TabIndex = 19;
-            this.lvDetalles.UseCompatibleStateImageBehavior = false;
-            this.lvDetalles.View = System.Windows.Forms.View.Details;
-            // 
-            // btnAgregar
-            // 
-            this.btnAgregar.Location = new System.Drawing.Point(684, 40);
-            this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(103, 29);
-            this.btnAgregar.TabIndex = 6;
-            this.btnAgregar.Text = "AGREGAR";
-            this.btnAgregar.UseVisualStyleBackColor = true;
-            // 
-            // btnSalir
-            // 
-            this.btnSalir.Location = new System.Drawing.Point(23, 629);
-            this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(72, 22);
-            this.btnSalir.TabIndex = 20;
-            this.btnSalir.Text = "SALIR";
-            this.btnSalir.UseVisualStyleBackColor = true;
-            // 
-            // columnHeader1
-            // 
-            this.columnHeader1.Text = "CANTIDAD";
-            this.columnHeader1.Width = 120;
-            // 
-            // columnHeader2
-            // 
-            this.columnHeader2.Text = "DESCRIPCION";
-            this.columnHeader2.Width = 360;
-            // 
-            // columnHeader3
-            // 
-            this.columnHeader3.Text = "PREC. UNIT.";
-            this.columnHeader3.Width = 120;
-            // 
-            // columnHeader4
-            // 
-            this.columnHeader4.Text = "IMPORTE";
-            this.columnHeader4.Width = 180;
             // 
             // columnHeader5
             // 
@@ -361,6 +221,151 @@
             this.columnHeader8.Text = "MONTO TOTAL";
             this.columnHeader8.Width = 240;
             // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(517, 391);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(39, 15);
+            this.lblTotal.TabIndex = 16;
+            this.lblTotal.Text = "TOTAL";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label14.Location = new System.Drawing.Point(698, 391);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(47, 15);
+            this.label14.TabIndex = 17;
+            this.label14.Text = "label14";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cboProductos);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.txtCantidad);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.txtPrecio);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Location = new System.Drawing.Point(14, 192);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(792, 90);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DATOS DEL PRODUCTO";
+            // 
+            // cboProductos
+            // 
+            this.cboProductos.FormattingEnabled = true;
+            this.cboProductos.Items.AddRange(new object[] {
+            "PS5 + 1 MANDO DS5 ",
+            "PS4(1TB) + 1 MANDO DS4",
+            "MANDO PS5/DS5",
+            "MANDO PS4/DS4"});
+            this.cboProductos.Location = new System.Drawing.Point(3, 44);
+            this.cboProductos.Name = "cboProductos";
+            this.cboProductos.Size = new System.Drawing.Size(121, 23);
+            this.cboProductos.TabIndex = 7;
+            this.cboProductos.Text = "(Seleccione)";
+            this.cboProductos.SelectedIndexChanged += new System.EventHandler(this.cboProductos_SelectedIndexChanged);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(684, 40);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(103, 29);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(527, 46);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(115, 23);
+            this.txtCantidad.TabIndex = 5;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(543, 19);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(65, 15);
+            this.label11.TabIndex = 4;
+            this.label11.Text = "CANTIDAD";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(275, 46);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(110, 23);
+            this.txtPrecio.TabIndex = 3;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(305, 21);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 15);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "PRECIO";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(8, 21);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(68, 15);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "PRODUCTO";
+            // 
+            // lvDetalles
+            // 
+            this.lvDetalles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lvDetalles.GridLines = true;
+            this.lvDetalles.Location = new System.Drawing.Point(7, 288);
+            this.lvDetalles.Name = "lvDetalles";
+            this.lvDetalles.Size = new System.Drawing.Size(781, 100);
+            this.lvDetalles.TabIndex = 19;
+            this.lvDetalles.UseCompatibleStateImageBehavior = false;
+            this.lvDetalles.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "CANTIDAD";
+            this.columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "DESCRIPCION";
+            this.columnHeader2.Width = 360;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "PREC. UNIT.";
+            this.columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "IMPORTE";
+            this.columnHeader4.Width = 180;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(23, 629);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(72, 22);
+            this.btnSalir.TabIndex = 20;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
             // frmBoleta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -370,7 +375,7 @@
             this.Controls.Add(this.lvDetalles);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label14);
-            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.lblTotal);
             this.Controls.Add(this.lvEstadistica);
             this.Controls.Add(this.label12);
             this.Controls.Add(this.btnRegistrar);
@@ -383,7 +388,7 @@
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblNumero);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "frmBoleta";
@@ -399,7 +404,7 @@
 
         private Label label1;
         private Label label2;
-        private Label label3;
+        private Label lblNumero;
         private Label label4;
         private Label label5;
         private Label label6;
@@ -412,14 +417,13 @@
         private Button btnRegistrar;
         private Label label12;
         private ListView lvEstadistica;
-        private Label btnTotal;
+        private Label lblTotal;
         private Label label14;
         private GroupBox groupBox1;
         private TextBox txtCantidad;
         private Label label11;
         private TextBox txtPrecio;
         private Label label10;
-        private ListBox cboProductos;
         private Label label9;
         private ListView lvDetalles;
         private Button btnAgregar;
@@ -432,5 +436,6 @@
         private ColumnHeader columnHeader6;
         private ColumnHeader columnHeader7;
         private ColumnHeader columnHeader8;
+        private ComboBox cboProductos;
     }
 }
--- pjBoletaVenta/frmBoleta.cs
+++ pjBoletaVenta/frmBoleta.cs
@@ -12,6 +12,9 @@ namespace pjBoletaVenta
 {
     public partial class frmBoleta : Form
     {
+        //Variables globales
+        int num;
+        Boleta objB = new();
         public frmBoleta()
         {
             InitializeComponent();
@@ -20,15 +23,128 @@ namespace pjBoletaVenta
         private void label11_Click(object sender, EventArgs e)
         {
 
+            num++;
+            lblNumero.Text = num.ToString("S5");
+            txtFecha.Text =DateTime.Now.ToShortDateString();
         }
 
-        private void label4_Click(object sender, EventArgs e)
+        
+        private void cboProductos_SelectedIndexChanged(object sender, EventArgs e)
         {
+            objB.DescripciónProducto = cboProductos.Text;
+            txtPrecio.Text = objB.determinaPrecio().ToString("C");
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            if (Valida() == "")
+            {
+                // capturar datos
+                CapturaDatos();
+                // determinar los calculos de la apalicacion
+                double precio = objB.determinaPrecio();
+                double importe = objB.calculaImporte();
+                // imprimir oos detalles de la venta
+                ImprimirDetalle();
+
+                // imprimir el total acumulado
+                lblTotal.Text = DeterminaTotal().ToString("C");
+          
+            }
+            else
+                MessageBox.Show($"El error se encuentra en {Valida()}");
+                
+                
+        }
+
+        private object DeterminaTotal()
+        {
+            double total = 0;
+            for (int i = 0; i < lvDetalles.Items.Count; i++)
+            {
+                total += double.Parse (lvDetalles.Items[i].SubItems[3].Text)
+            }
+            return total;
+        }
+
+        private void ImprimirDetalle()
+        {
+            ListViewItem fila = new(objB.CantidadComprada.ToString);
+            fila.SubItems.Add(objB.DescripciónProducto);
+            fila.SubItems.Add(precio.toString("0.00"));
+            fila.SubItems.Add(importe.toString("0.00"));
+            lvDetalles.Items.Add(fila);
+        }
+
+        private void CapturaDatos()
+        {
+            objB.NumeroBoleta = int.Parse(lblNumero.Text);
+            objB.NombreCliente = txtCliente.Text;
+            objB.DirecciónCliente = txtDireccion.Text;
+            objB.CédulaCliente = txtCedula.Text;
+            objB.FechaRegistro = DateTime.Parse(txtFecha.Text);
+            objB.DescripciónProducto = cboProductos.Text;
+            objB.CantidadComprada = int.Parse(txtCantidad.Text);
 
         }
 
-        private void label12_Click(object sender, EventArgs e)
+        private void valida()
         {
+            if (txtCliente.Text.Trim().Length == 0)
+            {
+                txtCliente.Focus();
+                return "nombre del cliente";
+
+            }
+            else if (txtDireccion.Text.Trim().Length == 0)
+            {
+                txtDireccion.Focus();
+                return "direccion del cliente";
+            }
+            else if (txtCedula.Text.Trim().Length == 0)
+            {
+                txtDireccion.Focus();
+                return "cedula del cliente";
+            }
+            else if (cboProductos.SelectedIndex == -1)
+            {
+                cboProductos.Focus();
+                return "descripcion del producto";
+            }
+            else if (txtCantidad.Text.Trim().Length == 0)
+            {
+                txtCantidad.Focus();
+                return "cantidad comprada";
+            }
+            else
+                return "";
+        }
+        private void btnRegistrar_Click(object sender , EventArgs e)
+        {
+            ListViewItem fila = new ListViewItem(lblNumero.Text);
+            fila.SubItems.Add(txtFecha.Text);
+            fila.SubItems.Add(TotalCantidad().ToString());
+            fila.SubItems.Add(DeterminaTotal().ToString());
+            lvEstadistica.Items.Add(fila);
+            LimpiarControles();
+
+        }
+        private void LimpiarControles()
+        {
+            num++;
+
+
+        }
+        // Total productos de boletas
+
+        private void TotalCantidad()
+        {
+            int total = 0;
+            for (int i = 0; i < lvEstadistica.Items.Count; i++)
+            {
+                total += int.Parse(lvDetalles.Items[i].SubItems[0].Text);
+            }
+            return total;
 
         }
     }
